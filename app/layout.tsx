8f205080d072bd292f456b3727a5128fe33074af
kasvrol <dovalekarolina@gmail.com>
2023-05-13 20:42:01 -0300

    first commit

---
+++ app/layout.tsx
@@ -0,0 +1,21 @@
+import "./globals.css";
+import { Inter } from "next/font/google";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata = {
+    title: "APOD NASA",
+    description: "Project APOD NASA using Next.js",
+};
+
+export default function RootLayout({
+    children,
+}: {
+    children: React.ReactNode;
+}) {
+    return (
+        <html lang="pt-BR">
+            <body className={inter.className}>{children}</body>
+        </html>
+    );
+}
