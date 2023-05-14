372edc1bd45ba407d81ef4006c06faf3c717a27a
kasvrol <dovalekarolina@gmail.com>
2023-05-14 12:16:34 -0300

    feat: diretório utils com arquivo interface. O arquivo guarda o tipo dos parametros recebidos pelos componentes e páginas

---
+++ app/utils/interface.ts
@@ -0,0 +1,20 @@
+export interface DateEntryProps {
+    inputDay: (date: string) => void;
+}
+
+export interface TextContainerProps {
+    explanation: string;
+    imageAuthor: string;
+    imageDate: string;
+    site: string;
+}
+
+export interface TitleContainerProps {
+    imageTitle: string;
+}
+
+export interface MediaProps {
+    image: string;
+    imageEntry: string;
+    title: string;
+}
