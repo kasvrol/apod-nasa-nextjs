f93aee1c6b28d3729bce24c7a6951d63cc088cdb
kasvrol <dovalekarolina@gmail.com>
2023-05-14 18:25:27 -0300

    ideia para carrossel, porém com erro do react em diversasd requisições simultanêas

--- app/middleware/index.ts
+++ app/middleware/index.ts
@@ -8,3 +8,36 @@ export async function ApiNasaGov(date: string) {
 
     return response;
 }
+
+export async function ApiNasaGovImage01(date: any) {
+    const response: any = await fetch(
+        `https://api.nasa.gov/planetary/apod?api_key=3oJxNtB9Kez0obSunOfeSpROAwbPU9pd6zCgYWIf&date=${date}`
+    );
+    if (!response.ok) {
+        throw new Error("Failed to fetch data");
+    }
+
+    return response;
+}
+
+export async function ApiNasaGovImage02(date: any) {
+    const response: any = await fetch(
+        `https://api.nasa.gov/planetary/apod?api_key=3oJxNtB9Kez0obSunOfeSpROAwbPU9pd6zCgYWIf&date=${date}`
+    );
+    if (!response.ok) {
+        throw new Error("Failed to fetch data");
+    }
+
+    return response;
+}
+
+export async function ApiNasaGovImage03(date: any) {
+    const response: any = await fetch(
+        `https://api.nasa.gov/planetary/apod?api_key=3oJxNtB9Kez0obSunOfeSpROAwbPU9pd6zCgYWIf&date=${date}`
+    );
+    if (!response.ok) {
+        throw new Error("Failed to fetch data");
+    }
+
+    return response;
+}
