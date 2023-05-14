dc2dff61a48426bcce70c9aa55ecd6f5a6d9933f
kasvrol <dovalekarolina@gmail.com>
2023-05-14 01:02:07 -0300

    feat: componente text que faz a renderização das imagens respondidos pela API

---
+++ app/components/media/index.tsx
@@ -0,0 +1,27 @@
+"use client";
+
+import styles from "../../styles/media.module.css";
+import ReactPlayer from "react-player";
+import { MediaProps } from "../../utils/interface";
+
+const Media: React.FC<MediaProps> = ({ image, imageEntry, title }) => {
+    const mediaType = (image: string) => {
+        if (image === "image") {
+            return (
+                <img
+                    src={imageEntry}
+                    className={styles.Image}
+                    alt={`Imagem sobre ${title}`}
+                />
+            );
+        } else {
+            return (
+                <ReactPlayer url={imageEntry} alt={`Vídeo sobre ${title}`} />
+            );
+        }
+    };
+
+    return <>{mediaType(image)}</>;
+};
+
+export default Media;
